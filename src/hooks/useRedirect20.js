79210b4605fa2cd2e6abf757075794076f8f3048
Mr. Hasan <hasanshahriar32@gmail.com>
2023-12-19 12:00:13 +0600

    updated timeout

--- src/hooks/useRedirect20.js
+++ src/hooks/useRedirect20.js
@@ -1,11 +1,11 @@
-"use client";
+
 const useRedirect20 = () => {
-    let Router;
-    if (typeof window !== "undefined") {
-      Router = require("next/router");
-    }
-  setTimeout(() => {
-    Router.push("/");
-  }, 20000);
+  if (typeof window !== "undefined") {
+    const router = require("next/router");
+    setTimeout(() => {
+      router.push("/");
+    }, 20000);
+  }
 };
+
 export default useRedirect20;
