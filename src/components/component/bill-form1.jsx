31af80478845d362ad9258a27126dfec1c88cfda
Mr. Hasan <hasanshahriar32@gmail.com>
2023-12-19 23:50:56 +0600

    Update language labels in Footer and BillForm1 components

--- src/components/component/bill-form1.jsx
+++ src/components/component/bill-form1.jsx
@@ -22,35 +22,35 @@ export function BillForm1() {
       <form className="mb-24 space-y-8 max-w-2xl mx-auto">
         <Card className="p-4 space-y-4">
           <CardHeader>
-            <h1 className="text-2xl font-bold">Date Selection</h1>
+            <h1 className="text-2xl font-bold">তারিখ নির্বাচন</h1>
           </CardHeader>
           <CardContent className="flex flex-col md:flex-row gap-4">
             <div className="w-full md:w-1/2">
-              <Label htmlFor="month">Month</Label>
+              <Label htmlFor="month">মাস</Label>
               <select className="select select-bordered w-full max-w-xs">
                 <option disabled selected>
-                  Select The Billing Month
+                  বিলিং মাস নির্বাচন করুন
                 </option>
                 <option>জানুয়ারি – January </option>
                 <option>ফেব্রুয়ারী – February </option>
                 <option>মার্চ – March </option>
-                <option>এপ্রিল -April </option>
+                <option>এপ্রিল – April </option>
                 <option>মে – May </option>
                 <option>জুন – June </option>
                 <option>জুলাই – July </option>
                 <option>আগষ্ট – August</option>
-                <option>সেপ্টেম্বর -September </option>
+                <option>সেপ্টেম্বর – September </option>
                 <option>অক্টোবর – October </option>
                 <option>নভেম্বর – November </option>
                 <option>ডিসেম্বর – December</option>
               </select>
             </div>
             <div className="w-full md:w-1/2">
-              <Label htmlFor="year">Year</Label>
+              <Label htmlFor="year">বছর</Label>
               <Input
                 className="w-full h-12"
                 id="year"
-                placeholder="Year"
+                placeholder="বছর নির্বাচন করুন"
                 type="number"
               />
             </div>
@@ -58,11 +58,11 @@ export function BillForm1() {
         </Card>
         <Card className="p-4 space-y-4">
           <CardHeader>
-            <h2 className="text-2xl font-bold">First Float Inputs</h2>
+            <h2 className="text-2xl font-bold">মেইন মিটারের তথ্য</h2>
           </CardHeader>
           <CardContent className="flex flex-col md:flex-row gap-4">
             <div className="w-full md:w-1/2">
-              <Label htmlFor="float1">First Float</Label>
+              <Label htmlFor="float1">মাসের শুরুতে ব্যালেন্স</Label>
               <Input
                 className="w-full"
                 id="float1"
@@ -72,7 +72,7 @@ export function BillForm1() {
               />
             </div>
             <div className="w-full md:w-1/2">
-              <Label htmlFor="float2">Second Float</Label>
+              <Label htmlFor="float2">মাসের শেষে ব্যালেন্স</Label>
               <Input
                 className="w-full"
                 id="float2"
@@ -85,11 +85,11 @@ export function BillForm1() {
         </Card>
         <Card className="p-4 space-y-4">
           <CardHeader>
-            <h2 className="text-2xl font-bold">Second Float Inputs</h2>
+            <h2 className="text-2xl font-bold">সাব মিটারের তথ্য</h2>
           </CardHeader>
           <CardContent className="flex flex-col md:flex-row gap-4">
             <div className="w-full md:w-1/2">
-              <Label htmlFor="float3">Third Float</Label>
+              <Label htmlFor="float3">মাসের শুরুতে ব্যালেন্স</Label>
               <Input
                 className="w-full"
                 id="float3"
@@ -99,7 +99,7 @@ export function BillForm1() {
               />
             </div>
             <div className="w-full md:w-1/2">
-              <Label htmlFor="float4">Fourth Float</Label>
+              <Label htmlFor="float4">মাসের শেষে ব্যালেন্স</Label>
               <Input
                 className="w-full"
                 id="float4"
@@ -116,7 +116,7 @@ export function BillForm1() {
             className="px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition-colors duration-200"
             type="submit"
           >
-            Next
+            পরবর্তী
           </Button>
         </div>
       </form>
